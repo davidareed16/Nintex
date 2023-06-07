3b9a01026934cc132f156c20e264c26777a8eaed
davidareed16 <davidareed16@gmail.com>
2023-06-07 12:27:36 -0500

    Update embedded-assuresign.js

--- embedded-assuresign.js
+++ embedded-assuresign.js
@@ -97,7 +97,7 @@ export class EmbeddedAssureSign extends LitElement {
             }
         };
 
-        const response = await fetch('https://account.assuresign.net/authentication/apiUser', {
+        const response = await fetch('https://account.assuresign.net/api/v3.7/authentication/apiUser', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
